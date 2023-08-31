6027ccfe3f3258a11a15a2c6928d3717a00642c6
JustLobo1121 <lobo1121personal@gmail.com>
2023-08-30 21:47:01 -0400

    first update - test

---
+++ assets/js/example.js
@@ -0,0 +1,15 @@
+
+// para quitar la parte de inicio y mostrar la cuenta👌
+function MostrarDatoCuentas () {
+    Body.innerHTML = headerDisplay + mainContainer
+    displayLogin.remove()
+}
+// funcion para iniciar sesion 👍
+function ConsultarDatos() {
+    muestraSaldo.innerHTML = '<p>Tiene un saldo de: ${saldo}</p>'
+    console.log("fun")
+}
+function Ingresar () {}
+function IngresoSaldo() {}
+function Retiro() {}
+function RetiroDeSaldo() {}
